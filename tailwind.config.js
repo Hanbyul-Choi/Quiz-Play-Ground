9773a5503d690d46cc3d3bae9df4235c93780c2b
heeyeon <godeeyeon@gmail.com>
2023-08-08 12:53:52 +0900

    Chore. 공통 작업: eslint 설정,  tailwind color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,29 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    colors: {
-      red: '#EB5353',
-      hoverRed: '#EA8282',
-      clickRed: '#B83535',
-      yellow: '#F9D923',
-      hoverYellow: '#FBE464',
-      clockYellow: '#B83535',
-      green: '#36AE7C',
-      hoverGreen: '#68CFA4',
-      clickGreen: '#409370',
-      blue: '#187498',
-      hoverBlue: '#4A9EBE',
-      clickBlue: '#1D5F79',
-      skyblue: '#3AB4F2',
-      clickSkyblue: '#4F9EC6',
-      hoverSkyblue: '#82CDF4',
-      gray: '#9E9D9D',
-      clickGray: '#777777'
-    },
-    extend: {}
+    extend: {
+      colors: {
+        red: '#EB5353',
+        hoverRed: '#EA8282',
+        clickRed: '#B83535',
+        yellow: '#F9D923',
+        hoverYellow: '#FBE464',
+        clockYellow: '#B83535',
+        green: '#36AE7C',
+        hoverGreen: '#68CFA4',
+        clickGreen: '#409370',
+        blue: '#187498',
+        hoverBlue: '#4A9EBE',
+        clickBlue: '#1D5F79',
+        skyBlue: '#3AB4F2',
+        clickSkyBlue: '#4F9EC6',
+        hoverSkyBlue: '#82CDF4',
+        gray1: '#F0F0F0',
+        gray2: '#D9D9D9',
+        gray3: '#9E9D9D',
+        gray4: '#777777'
+      }
+    }
   },
   plugins: []
 };
