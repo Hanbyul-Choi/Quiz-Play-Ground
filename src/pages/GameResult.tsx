8d91938cc81754f6a013e8eba5bedf8138b3460e
heeyeon <godeeyeon@gmail.com>
2023-08-09 15:54:23 +0900

    Feat. 게임 결과 페이지 UI 완성

--- src/pages/GameResult.tsx
+++ src/pages/GameResult.tsx
@@ -1,3 +1,11 @@
+import Comment from '../components/gameResult/Comment';
+import Result from '../components/gameResult/Result';
+
 export const GameResult = () => {
-  return <div>GameResult</div>;
+  return (
+    <div className="flex justify-center items-start" style={{ marginTop: '18vh' }}>
+      <Result />
+      <Comment />
+    </div>
+  );
 };
